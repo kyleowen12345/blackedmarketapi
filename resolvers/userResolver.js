bd5eb28927c6202b31fe65cacf31627938caa456
kyleowen12345 <gakyleowen@gmail.com>
2021-10-11 13:48:15 +0800

    jwt error

--- resolvers/userResolver.js
+++ resolvers/userResolver.js
@@ -146,7 +146,7 @@ export default {
 
       const user = await userModel.create({name, email, password:hashedPassword });
 
-      const token = jwt.sign({ id: user.id,email:user.email,name:user.name,profilePic:user.profilePic }, 'riddlemethis', { expiresIn: '24h'  });
+      const token = jwt.sign({ id: user.id,email:user.email,name:user.name,profilePic:user.profilePic }, process.env.SECRET_KEY, { expiresIn: '24h'  });
 
       return {token}
       
@@ -169,7 +169,7 @@ export default {
         throw new AuthenticationError('Invalid credentials');
       }
 
-      const token = jwt.sign({ id: user.id,email:user.email,name:user.name,profilePic:user.profilePic }, 'riddlemethis', { expiresIn: '24h'  });
+      const token = jwt.sign({ id: user.id,email:user.email,name:user.name,profilePic:user.profilePic }, process.env.SECRET_KEY, { expiresIn: '24h'  });
 
       return {token};
     },
